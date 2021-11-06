afdd4086a58a03ee83d898bf4942f26d20484d39
UnicycleDumpTruck <junk@jeffhighsmith.com>
2021-11-05 21:47:53 -0400

    added methods to classes to setup game

--- battleship.py
+++ battleship.py
@@ -26,8 +26,20 @@ GRID_SIZE = 10
 class Grid:
     def __init__(self, name):
         self.name = name
-        self.grid = [[[None] for i in range(GRID_SIZE)]
+        self.grid = [["w" for i in range(GRID_SIZE)]
                      for j in range(GRID_SIZE)]
+
+    def printGrid(self):
+        print(self.name)
+        for row in self.grid:
+            for column in row:
+                print(column + " ", end="")
+            print("")
+        print("")
+
+    def addShip(self, coords):
+        self.grid[coords[0]][coords[1]] = str(coords[2])
+        self.printGrid()
 
 
 class Ship:
@@ -43,6 +55,15 @@ class Ship:
     def printShip(self):
         print(self.ship_type)
 
+    def deployShip(self):
+        print(f"Deploying a {self.ship_type} of size {self.ship_size}:")
+        orientation = ""
+        while not (orientation == "h" or orientation == "v"):
+            orientation = input("Horizontal or Vertical ('h' or 'v')? ")
+        row = int(input("Row (1 through 10)? "))
+        column = int(input("Column (1 through 10)? "))
+        return (row, column, self.ship_size)
+
 
 class Player:
     def __init__(self, name):
@@ -54,15 +75,35 @@ class Player:
             Ship("Destroyer", 3),
             Ship("Patrol Boat", 2),
         ]
+        self.fleetGrid = Grid(self.name + " Fleet")
+        self.shotGrid = Grid(self.name + " Shots")
 
     def printPlayer(self):
         print(self.name)
         for ship in self.ships:
             ship.printShip()
+        self.fleetGrid.printGrid()
+        self.shotGrid.printGrid()
+
+    def deployFleet(self):
+        for ship in self.ships:
+            self.fleetGrid.addShip(ship.deployShip())
 
 
-player_a = Player("Player A")
-player_b = Player("Player B")
+class Game:
+    def __init__(self):
+        self.player_a = Player("Player A")
+        self.player_b = Player("Player B")
 
-player_a.printPlayer()
-player_b.printPlayer()
+    def setup_fleets(self):
+        self.player_a.deployFleet()
+        self.player_b.deployFleet()
+
+
+game = Game()
+game.setup_fleets()
+
+# Game starts
+# Player A places fleet: orientation, move with arrows
+# Player B places fleet:
+# Take turns taking shots
